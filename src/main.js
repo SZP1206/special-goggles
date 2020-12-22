31233283057bd8b2dc3227e045acfbc3134785d9
SZP1206 <43406070+SZP1206@users.noreply.github.com>
2020-12-22 18:48:31 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import Vant from 'vant'
-import 'vant/lib/button/style'
+import 'vant/lib/index.css'
 
 import 'amfe-flexible'
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-container">Login</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped></style>
