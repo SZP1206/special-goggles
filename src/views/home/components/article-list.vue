34fef8e19e9ba6915c668e27e62a718ab59a1597
SZP1206 <43406070+SZP1206@users.noreply.github.com>
2020-12-23 20:16:53 +0800

    文章列表组件基础布局

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="article-list">
-    {{ channel.name }}
+    <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="我是有底线的"
+        @load="onLoad"
+      >
+        <van-cell v-for="item in list" :key="item" :title="item" />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -15,14 +24,55 @@ export default {
     },
   },
   data() {
-    return {}
+    return {
+      list: [],
+      loading: false,
+      finished: false,
+      refreshing: false,
+    }
   },
   computed: {},
   watch: {},
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    onLoad() {
+      setTimeout(() => {
+        if (this.refreshing) {
+          this.list = []
+          this.refreshing = false
+        }
+
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        this.loading = false
+
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 1000)
+    },
+    onRefresh() {
+      // 清空列表数据
+      this.finished = false
+
+      // 重新加载数据
+      // 将 loading 设置为 true，表示处于加载状态
+      this.loading = true
+      this.onLoad()
+    },
+  },
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.article-list {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 90px;
+  bottom: 46px;
+  overflow-y: auto;
+}
+</style>
