149165fa1c9496d4caac86d86b5d34e539ae39c0
SZP1206 <43406070+SZP1206@users.noreply.github.com>
2020-12-24 17:27:58 +0800

    频道编辑页面基础布局

--- src/views/home/components/channel-edit.vue
+++ src/views/home/components/channel-edit.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="channel-edit">
-    ChannelEdit
+    <van-cell>
+      <template #title> 我的频道 </template>
+      <template>
+        <van-button class="editBtn" type="danger" plain size="small" round >编辑</van-button>
+      </template>
+    </van-cell>
+
+    <van-grid :gutter="10">
+      <van-grid-item v-for="(value, index) in 8" :key="index" text="文字" />
+    </van-grid>
+
+    <van-cell>
+      <template #title> 推荐频道 </template>
+    </van-cell>
+
+    <van-grid :gutter="10">
+      <van-grid-item v-for="(value, index) in 8" :key="index" text="文字" />
+    </van-grid>
   </div>
 </template>
 
@@ -20,4 +37,29 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.channel-edit {
+  padding-top: 60px;
+
+  .editBtn{
+    width: 64px;
+  }
+
+  .van-cell__title {
+    font-size: 16px;
+    color: #333333;
+  }
+
+  .van-grid-item {
+    width: 80px;
+    height: 43px;
+    /deep/ .van-grid-item__content {
+      background-color: #f4f5f6;
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #222;
+      }
+    }
+  }
+}
+</style>
