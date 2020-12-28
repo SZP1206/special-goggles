eb09beb3ba702d60de27f374fb7b374f792ffd5b
SZP1206 <43406070+SZP1206@users.noreply.github.com>
2020-12-28 15:24:30 +0800

    创建文章详情组件，配置动态路由

---
+++ src/views/article/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="article-container">
+    Article
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticleIndex',
+  components: {},
+  props: {
+    articleId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped></style>
